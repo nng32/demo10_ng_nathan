e77ba86aa9caeeb43153a37ffd776bd240a1600c
nng32 <nng32@my.bcit.ca>
2022-03-08 13:25:17 -0800

    Completed demo

---
+++ scripts/profile.js
@@ -0,0 +1,67 @@
+var currentUser          //put this right after you start script tag before writing any functions.
+
+function populateInfo() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid)
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    //get the data fields of the user
+                    var userName = userDoc.data().name;
+                    var userSchool = userDoc.data().school;
+                    var userCity = userDoc.data().city;
+
+                    //if the data fields are not empty, then write them in to the form.
+                    if (userName != null) {
+                        document.getElementById("nameInput").value = userName;
+                    }
+                    if (userSchool != null) {
+                        document.getElementById("schoolInput").value = userSchool;
+                    }
+                    if (userCity != null) {
+                        document.getElementById("cityInput").value = userCity;
+                    }
+                })
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+        }
+    });
+}
+
+function editUserInfo() {
+    //Enable the form fields
+    document.getElementById('personalInfoFields').disabled = false;
+}
+
+function saveUserInfo() {
+    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
+    userSchool = document.getElementById('schoolInput').value;     //get the value of the field with id="schoolInput"
+    userCity = document.getElementById('cityInput').value;       //get the value of the field with id="cityInput"
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid);
+
+            // write to database
+            currentUser.update({
+                name: userName,
+                school: userSchool,
+                city: userCity
+            }).then (() => {
+                alert("Document has been updated");
+                document.getElementById('personalInfoFields').disabled = true;
+            })
+        }
+        else {
+            alert("Failed to retrieve user. Check to make sure you are signed in.");
+        }
+    })
+}
+
+//call the function to run it 
+populateInfo();
